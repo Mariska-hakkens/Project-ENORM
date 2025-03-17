f6828c47ee6aed88904d0b0397fe01812364e479
Mariska-hakkens <515278@student.fontys.nl>
2025-03-17 11:09:32 +0100

    Eerste versie enofight

---
+++ js/main.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let naam = "EnoRm";
+    console.log(naam);
+
+    const hoverText = document.getElementById("hoverText");
+    const hond = document.getElementById("hond");
+    const blikje = document.getElementById("blikje");
+
+    // Hond verschijnt bij hover en blijft zichtbaar
+    hoverText.addEventListener("mouseenter", function () {
+        hond.style.display = "block";
+    });
+
+    // Blikje verschijnt bij klik
+    document.addEventListener("click", function () {
+        blikje.style.display = "block";
+
+        setTimeout(function () {
+            alert("De hond drinkt het blikje EnoRm leeg...");
+            window.location.href = "https://fightclub.nl";
+        }, 2000);
+    });
+});
